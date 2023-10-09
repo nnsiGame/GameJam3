cef03b720e06a12dcce0366a5944d13c088a97ba
Izumi <g021g1281@g.neec.ac.jp>
2023-10-09 13:35:56 +0900

    切り替えで消える床用のプログラム完成

--- GameJamProject/Assets/Program/ChangeFloor.cs
+++ GameJamProject/Assets/Program/ChangeFloor.cs
@@ -4,15 +4,47 @@ using UnityEngine;
 
 public class ChangeFloor : MonoBehaviour
 {
+    [SerializeField] bool m_Orange;
+    BGMManager m_BGMMnager;
+    Collider2D m_Collider;
+    SpriteRenderer m_SpriteRender;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        m_BGMMnager = GameObject.FindWithTag("BGMManager").GetComponent<BGMManager>();
+        m_Collider = GetComponent<Collider2D>();
+        m_SpriteRender = GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (m_BGMMnager.m_ABGM)
+        {
+            if (m_Orange)
+            {
+                m_Collider.enabled = true;
+                m_SpriteRender.color = new Color(m_SpriteRender.color.r, m_SpriteRender.color.g, m_SpriteRender.color.b, 1);
+            }
+            else 
+            {
+                m_Collider.enabled = false;
+                m_SpriteRender.color = new Color(m_SpriteRender.color.r, m_SpriteRender.color.g, m_SpriteRender.color.b, 0.1f);
+            }
+        }
+        else
+        {
+            if (m_Orange)
+            {
+                m_Collider.enabled = false;
+                m_SpriteRender.color = new Color(m_SpriteRender.color.r, m_SpriteRender.color.g, m_SpriteRender.color.b, 0.1f);
+            }
+            else
+            {
+                m_Collider.enabled = true;
+                m_SpriteRender.color = new Color(m_SpriteRender.color.r, m_SpriteRender.color.g, m_SpriteRender.color.b, 1);
+            }
+        }
     }
 }
