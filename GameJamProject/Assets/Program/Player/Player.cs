7384ef77e3473b4443b7be478a77002950be746d
Izumi <g021g1281@g.neec.ac.jp>
2023-10-09 09:03:45 +0900

    PlayerのPrefabを追加

--- GameJamProject/Assets/Program/Player/Player.cs
+++ GameJamProject/Assets/Program/Player/Player.cs
@@ -10,6 +10,7 @@ public class Player : MonoBehaviour
     [SerializeField] float m_JumpPower = 5f;            // ƒWƒƒƒ“ƒv—Í
 
     bool m_IsJump;
+    bool m_CanChangeBGM;
 
     [SerializeField] GameObject m_AMagic;
     [SerializeField] GameObject m_BMagic;
@@ -21,7 +22,7 @@ public class Player : MonoBehaviour
 
     Animator m_Animator;
 
-    BGMManager m_SceneManager;
+    BGMManager m_BGMManager;
 
     State m_CurrentState;
     enum State
@@ -34,9 +35,10 @@ public class Player : MonoBehaviour
     void Start()
     {
         m_IsJump = false;
+        m_CanChangeBGM = false;
 
         m_CurrentState = State.Normal;
-        m_SceneManager = GameObject.FindWithTag("SceneManager").GetComponent<BGMManager>();
+        m_BGMManager = GameObject.FindWithTag("SceneManager").GetComponent<BGMManager>();
         m_CurrentSpeed = 0;
         m_RB = GetComponent<Rigidbody2D>();
 
@@ -49,7 +51,9 @@ public class Player : MonoBehaviour
         {
             Jump();
             bool attack = Input.GetMouseButtonDown(0);
-            if (attack) Instantiate(m_SceneManager.m_ABGM ? m_AMagic : m_BMagic, m_MagicCreatePoint.position, m_MagicCreatePoint.transform.rotation);
+            if (attack) Instantiate(m_BGMManager.m_ABGM ? m_AMagic : m_BMagic, m_MagicCreatePoint.position, m_MagicCreatePoint.transform.rotation);
+
+            if (Input.GetButtonDown("ChangeBGM") && m_CanChangeBGM) m_BGMManager.ChangeBGM();
         }
     }
 
@@ -136,5 +140,15 @@ public class Player : MonoBehaviour
     private void OnDrawGizmos()
     {
         Gizmos.DrawCube(m_BoxCastOrigin.position, new Vector2(1, 1));
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("ChangeBGMPoint")) m_CanChangeBGM = true;
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("ChangeBGMPoint")) m_CanChangeBGM = false;
     }
 }
